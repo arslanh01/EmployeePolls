0c0acc7a18047b84e9bce44bc47e629975b69e80
arslanh01 <ah644635@gmail.com>
2022-07-21 14:36:38 +0500

    fix: Navbar fix

--- project-code/src/components/Nav.js
+++ project-code/src/components/Nav.js
@@ -9,25 +9,40 @@ const Nav = (props) => {
     navigate("/");
   };
   return (
-    <nav className="nav" data-testid="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/leaderboard">Leaderboard</Link>
-        </li>
-        <li>
-          <Link to="/add" data-testid="create-btn">
-            Create New Poll
-          </Link>
-        </li>
-        <li onClick={handleLogout} className="logout-btn">
-          Logout
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="nav" data-testid="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/leaderboard">Leaderboard</Link>
+          </li>
+          <li>
+            <Link to="/add" data-testid="create-btn">
+              Create New Poll
+            </Link>
+          </li>
+          <li onClick={handleLogout} className="logout-btn">
+            Logout
+          </li>
+        </ul>
+      </nav>
+      <div className="center">
+        <h3>
+          {props.user.name} ({props.loginUser})
+        </h3>
+      </div>
+    </div>
   );
 };
 
-export default connect()(Nav);
+const mapStateToProps = ({ users, loginUser }) => {
+  const user = users[loginUser];
+  return {
+    user,
+    loginUser,
+  };
+};
+
+export default connect(mapStateToProps)(Nav);
